be100f35217428c659a63576c1cabd1ae67b8cca
Nikuez <47661097+Nikuez95@users.noreply.github.com>
2021-08-05 22:14:23 +0200

    Add files via upload
    • Fixed the carjack, now mechanic don't have a problems to remove that
    • Removed the Menu, now start repainting immediately
    • Optimized

---
+++ client/main.lua
@@ -0,0 +1,167 @@
+
+ObjectInFront = function(ped, pos)
+	local entityWorld = GetOffsetFromEntityInWorldCoords(ped, 0.0, 1.5, 0.0)
+	local car = CastRayPointToPoint(pos.x, pos.y, pos.z, entityWorld.x, entityWorld.y, entityWorld.z, 30, ped, 0)
+	local _, _, _, _, result = GetRaycastResult(car)
+	return result
+end
+
+
+GetVehicleInDirection = function()
+	local playerPed    = PlayerPedId()
+	local playerCoords = GetEntityCoords(playerPed)
+	local inDirection  = GetOffsetFromEntityInWorldCoords(playerPed, 0.0, 5.0, 0.0)
+	local rayHandle    = StartShapeTestRay(playerCoords, inDirection, 10, playerPed, 0)
+	local numRayHandle, hit, endCoords, surfaceNormal, entityHit = GetShapeTestResult(rayHandle)
+
+	if hit == 1 and GetEntityType(entityHit) == 2 then
+		return entityHit
+	end
+
+	return nil
+end
+
+doAction = function(veh)
+	TriggerServerEvent("nk-repair:removeitem", "kitavanzato", 1)
+
+	local ped = PlayerPedId()
+		local coords = GetEntityCoords(ped)
+	local dict
+	local model = 'prop_carjack'
+	local offset = GetOffsetFromEntityInWorldCoords(ped, 0.0, -2.0, 0.0)
+	local headin = GetEntityHeading(ped)
+	
+	local vehicle   = GetVehicleInDirection()
+	FreezeEntityPosition(veh, true)
+	local vehpos = GetEntityCoords(veh)
+	dict = 'mp_car_bomb'
+	RequestAnimDict(dict)
+	RequestModel(model)
+	while not HasAnimDictLoaded(dict) or not HasModelLoaded(model) do
+		Citizen.Wait(1)
+	end
+	local vehjack = CreateObject(GetHashKey(model), vehpos.x, vehpos.y, vehpos.z - 0.5, true, true, true)
+	exports['progressBars']:startUI(9250, "Positioning the car jack")
+	
+	AttachEntityToEntity(vehjack, veh, 0, 0.0, 0.0, -1.0, 0.0, 0.0, 0.0, false, false, false, false, 0, true)
+	TaskPlayAnimAdvanced(ped, dict, 'car_bomb_mechanic', coords, 0.0, 0.0, headin, 1.0, 0.5, 1250, 1, 0.0, 1, 1)
+	Citizen.Wait(1250)
+	SetEntityCoordsNoOffset(veh, vehpos.x, vehpos.y, vehpos.z + 0.01, true, true, true)
+	TaskPlayAnimAdvanced(ped, dict, 'car_bomb_mechanic', coords, 0.0, 0.0, headin, 1.0, 0.5, 1000, 1, 0.25, 1, 1)
+	Citizen.Wait(1000)
+	SetEntityCoordsNoOffset(veh, vehpos.x, vehpos.y, vehpos.z + 0.025, true, true, true)
+	TaskPlayAnimAdvanced(ped, dict, 'car_bomb_mechanic', coords, 0.0, 0.0, headin, 1.0, 0.5, 1000, 1, 0.25, 1, 1)
+	Citizen.Wait(1000)
+	SetEntityCoordsNoOffset(veh, vehpos.x, vehpos.y, vehpos.z + 0.05, true, true, true)
+	TaskPlayAnimAdvanced(ped, dict, 'car_bomb_mechanic', coords, 0.0, 0.0, headin, 1.0, 0.5, 1000, 1, 0.25, 1, 1)
+	Citizen.Wait(1000)
+	SetEntityCoordsNoOffset(veh, vehpos.x, vehpos.y, vehpos.z + 0.1, true, true, true)
+	TaskPlayAnimAdvanced(ped, dict, 'car_bomb_mechanic', coords, 0.0, 0.0, headin, 1.0, 0.5, 1000, 1, 0.25, 1, 1)
+	Citizen.Wait(1000)
+	SetEntityCoordsNoOffset(veh, vehpos.x, vehpos.y, vehpos.z + 0.15, true, true, true)
+	TaskPlayAnimAdvanced(ped, dict, 'car_bomb_mechanic', coords, 0.0, 0.0, headin, 1.0, 0.5, 1000, 1, 0.25, 1, 1)
+	Citizen.Wait(1000)
+	SetEntityCoordsNoOffset(veh, vehpos.x, vehpos.y, vehpos.z + 0.2, true, true, true)
+	TaskPlayAnimAdvanced(ped, dict, 'car_bomb_mechanic', coords, 0.0, 0.0, headin, 1.0, 0.5, 1000, 1, 0.25, 1, 1)
+	Citizen.Wait(1000)
+	SetEntityCoordsNoOffset(veh, vehpos.x, vehpos.y, vehpos.z + 0.3, true, true, true)
+	TaskPlayAnimAdvanced(ped, dict, 'car_bomb_mechanic', coords, 0.0, 0.0, headin, 1.0, 0.5, 1000, 1, 0.25, 1, 1)
+	dict = 'move_crawl'
+	Citizen.Wait(1000)
+	SetEntityCoordsNoOffset(veh, vehpos.x, vehpos.y, vehpos.z + 0.4, true, true, true)
+	TaskPlayAnimAdvanced(ped, dict, 'car_bomb_mechanic', coords, 0.0, 0.0, headin, 1.0, 0.5, 1000, 1, 0.25, 1, 1)
+	SetEntityCoordsNoOffset(veh, vehpos.x, vehpos.y, vehpos.z + 0.5, true, true, true)
+	SetEntityCollision(veh, false, false)
+	TaskPedSlideToCoord(ped, offset, headin, 1000)
+	Citizen.Wait(1000)
+	
+	
+	RequestAnimDict(dict)
+	while not HasAnimDictLoaded(dict) do
+		Citizen.Wait(100)
+	end
+	exports['progressBars']:startUI(11000, "Fixing the vehicle")
+	TaskPlayAnimAdvanced(ped, dict, 'onback_bwd', coords, 0.0, 0.0, headin - 180, 1.0, 0.5, 3000, 1, 0.0, 1, 1)
+	dict = 'amb@world_human_vehicle_mechanic@male@base'
+	Citizen.Wait(3000)
+	RequestAnimDict(dict)
+	while not HasAnimDictLoaded(dict) do
+		Citizen.Wait(1)
+	end
+	TaskPlayAnim(ped, dict, 'base', 8.0, -8.0, 5000, 1, 0, false, false, false)
+	dict = 'move_crawl'
+	Citizen.Wait(5000)
+	local coords2 = GetEntityCoords(ped)
+	RequestAnimDict(dict)
+	while not HasAnimDictLoaded(dict) do
+		Citizen.Wait(1)
+	end
+	TaskPlayAnimAdvanced(ped, dict, 'onback_fwd', coords2, 0.0, 0.0, headin - 180, 1.0, 0.5, 2000, 1, 0.0, 1, 1)
+	Citizen.Wait(3000)
+	dict = 'mp_car_bomb'
+	RequestAnimDict(dict)
+	while not HasAnimDictLoaded(dict) do
+		Citizen.Wait(1)
+	end
+	SetVehicleFixed(vehicle)
+	SetVehicleDeformationFixed(vehicle)
+	SetVehicleUndriveable(vehicle, false)
+	SetVehicleEngineOn(vehicle, true, true)
+	ClearPedTasksImmediately(playerPed)
+	exports['progressBars']:startUI(8250, "Lifting the car jack")
+	TaskPlayAnimAdvanced(ped, dict, 'car_bomb_mechanic', coords, 0.0, 0.0, headin, 1.0, 0.5, 1250, 1, 0.0, 1, 1)
+	Citizen.Wait(1250)
+	SetEntityCoordsNoOffset(veh, vehpos.x, vehpos.y, vehpos.z + 0.4, true, true, true)
+	TaskPlayAnimAdvanced(ped, dict, 'car_bomb_mechanic', coords, 0.0, 0.0, headin, 1.0, 0.5, 1000, 1, 0.25, 1, 1)
+	Citizen.Wait(1000)
+	SetEntityCoordsNoOffset(veh, vehpos.x, vehpos.y, vehpos.z + 0.3, true, true, true)
+	TaskPlayAnimAdvanced(ped, dict, 'car_bomb_mechanic', coords, 0.0, 0.0, headin, 1.0, 0.5, 1000, 1, 0.25, 1, 1)
+	Citizen.Wait(1000)
+	SetEntityCoordsNoOffset(veh, vehpos.x, vehpos.y, vehpos.z + 0.2, true, true, true)
+	TaskPlayAnimAdvanced(ped, dict, 'car_bomb_mechanic', coords, 0.0, 0.0, headin, 1.0, 0.5, 1000, 1, 0.25, 1, 1)
+	Citizen.Wait(1000)
+	SetEntityCoordsNoOffset(veh, vehpos.x, vehpos.y, vehpos.z + 0.15, true, true, true)
+	TaskPlayAnimAdvanced(ped, dict, 'car_bomb_mechanic', coords, 0.0, 0.0, headin, 1.0, 0.5, 1000, 1, 0.25, 1, 1)
+	Citizen.Wait(1000)
+	SetEntityCoordsNoOffset(veh, vehpos.x, vehpos.y, vehpos.z + 0.1, true, true, true)
+	TaskPlayAnimAdvanced(ped, dict, 'car_bomb_mechanic', coords, 0.0, 0.0, headin, 1.0, 0.5, 1000, 1, 0.25, 1, 1)
+	Citizen.Wait(1000)
+	SetEntityCoordsNoOffset(veh, vehpos.x, vehpos.y, vehpos.z + 0.05, true, true, true)
+	TaskPlayAnimAdvanced(ped, dict, 'car_bomb_mechanic', coords, 0.0, 0.0, headin, 1.0, 0.5, 1000, 1, 0.25, 1, 1)
+	Citizen.Wait(1000)
+	SetEntityCoordsNoOffset(veh, vehpos.x, vehpos.y, vehpos.z + 0.025, true, true, true)
+	TaskPlayAnimAdvanced(ped, dict, 'car_bomb_mechanic', coords, 0.0, 0.0, headin, 1.0, 0.5, 1000, 1, 0.25, 1, 1)
+	dict = 'move_crawl'
+	Citizen.Wait(1000)
+	SetEntityCoordsNoOffset(veh, vehpos.x, vehpos.y, vehpos.z + 0.01, true, true, true)
+	TaskPlayAnimAdvanced(ped, dict, 'car_bomb_mechanic', coords, 0.0, 0.0, headin, 1.0, 0.5, 1000, 1, 0.25, 1, 1)
+	SetEntityCoordsNoOffset(veh, vehpos.x, vehpos.y, vehpos.z, true, true, true)
+	FreezeEntityPosition(veh, false)
+		Citizen.Wait(100)
+		DetachEntity(vehjack, true, false)
+		SetEntityCollision(vehjack, false, false)
+		DeleteEntity(vehjack)
+		
+
+	SetEntityCollision(veh, true, true)
+	
+
+end
+
+
+RegisterNetEvent("nk-repair:client:RepairVeh")
+AddEventHandler("nk-repair:client:RepairVeh", function()
+
+		local ped = PlayerPedId()
+		local coords = GetEntityCoords(ped)
+		local veh = ObjectInFront(ped, coords)
+		if DoesEntityExist(veh) then
+			if IsEntityAVehicle(veh) and not IsThisModelABike(veh) then
+
+					doAction(veh)
+			else
+				ESX.showNotification('You can\'t repair this Vehicle with this Kit.')
+			end
+
+		end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+
+fx_version 'bodacious'
+game 'gta5'
+
+description 'Created by NK-Development [Nikuez#5769]'
+
+client_scripts {
+	'client/*.lua',
+
+}
+
+server_scripts {
+	'server/*.lua',
+    
+}
